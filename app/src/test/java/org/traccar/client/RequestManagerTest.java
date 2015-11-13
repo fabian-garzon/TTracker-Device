0049f12b4acc04e19d1d04d7ccd862f5ee081160
fabian-garzon <monse40@gmail.com>
2015-11-13 10:08:33 -0500

    Primer commit

---
+++ app/src/test/java/org/traccar/client/RequestManagerTest.java
@@ -0,0 +1,25 @@
+
+package org.ttracker.client;
+
+import com.ttracker.client.BuildConfig;
+import com.ttracker.client.RequestManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class RequestManagerTest {
+
+    @Test
+    public void testSendRequest() throws Exception {
+
+        assertTrue(RequestManager.sendRequest("http://www.google.com"));
+
+    }
+
+}
